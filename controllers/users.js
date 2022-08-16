bebb0fd3bf7895878714a6c18e0eddf0ccbbe5ad
amandeep2508 <amandeep.chauhan@cogoport.com>
2022-08-16 17:24:36 +0530

    es lint

--- controllers/users.js
+++ controllers/users.js
@@ -1,46 +1,45 @@
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid"
 
-const users  =[]
+const users = []
 
 //createuser
 export const createUser = (req, res) => {
-	const body = req.body;
+  const body = req.body
 
-	users.push({...body, id: uuid()})
-	res.send("User added successfully");
-};
+  users.push({ ...body, id: uuid() })
+  res.send("User added successfully")
+}
 
 //getusers
 export const getUsers = (req, res) => {
-	res.send(users);
-};
+  res.send(users)
+}
 
 //get single users
 export const getUser = (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params
 
-	const user = users.filter((user) => user.id === id);
-	res.send(user);
-};
+  const user = users.filter((user) => user.id === id)
+  res.send(user)
+}
 
 //delete users
 export const deleteUser = (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params
 
-	const user = users.filter((user) => user.id !== id);
-	res.send("User deleted successfully");
-};
+  const user = users.filter((user) => user.id !== id)
+  res.send("User deleted successfully")
+}
 
 //update users
 export const updateUser = (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params
 
-	const user = users.find((user) => user.id === id);
+  const user = users.find((user) => user.id === id)
 
-	user.name = req.body.name;
-	user.email = req.body.email;
-	user.contact = req.body.contact;
+  user.name = req.body.name
+  user.email = req.body.email
+  user.contact = req.body.contact
 
-	res.send("User updated successfully");
-};
-
+  res.send("User updated successfully")
+}
